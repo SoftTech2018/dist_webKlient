a8aa69928e227238b8d60371d6516f2fe1da7d14
camillaolsen <camilla.braae.olsen1@gmail.com>
2016-05-03 09:47:47 +0200

    sende/modtager nu spots fra database

--- web/VisSpotController.js
+++ web/VisSpotController.js
@@ -9,17 +9,21 @@
 var app = angular.module('jsonapp', ['ngRoute', 'booleanFilter','ngAnimate','ui.bootstrap']);
 
 app.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'sider/VisSpots.html',
-      controller: 'jsonController'
+    $routeProvider
+            .when('/', {
+                templateUrl: 'sider/VisSpots.html',
+        controller: 'jsonController'
     }).when('/Opret', {
-      templateUrl: 'sider/OpretSpots.html',
-      controller: 'spotController'
+        templateUrl: 'sider/OpretSpots.html',
+        controller: 'spotController'
     }).otherwise ({
-    redirectTo: 'sider/VisSpots.html'
+        redirectTo: 'sider/VisSpots.html'
+    });
 });
-});
+
+
+
+
 
 
 app.controller('jsonController', function($http,$scope){
@@ -28,8 +32,7 @@ app.controller('jsonController', function($http,$scope){
         url: 'http://localhost:8080/ConvoyServer/webresources/convoy/get_all'
     }).success(function(data){
         $scope.data = data;    
-        console.log(typeof($scope.data));
-        $scope.id = data[0].name;
+        console.log($scope.data);
         
        
         console.log(typeof($scope.id));
@@ -49,8 +52,11 @@ angular.module('booleanFilter', []).filter('checkmark', function() {
         }
         else { return input;
         }
-        };
-    });
+    };
+});
+
+
+
 
 app.controller('spotController', function($scope, $http){
    
@@ -58,41 +64,38 @@ app.controller('spotController', function($scope, $http){
     $scope.spot = {};
     
     //Default værdier til objekt. Overskrives når form submittes. 
-    $scope.spot.id = 0;
-    $scope.spot.toilet = false;
-    $scope.spot.bad = false;
-    $scope.spot.mad = false;
-    $scope.spot.seng = false;
-    $scope.spot.tank = false;
-    $scope.spot.ab = false;
-    $scope.spot.vogntog = false; 
+   
+    $scope.spot.wc = false;
+    $scope.spot.bath = false;
+    $scope.spot.food = false;
+    $scope.spot.bed = false;
+    $scope.spot.fuel = false;
+    $scope.spot.addBlue = false;
+    $scope.spot.roadtrain = false; 
     $scope.spot.longitude = "";
     $scope.spot.latitude = "";
     
     
     $scope.getAdress = function(val) {
         console.log("getADr er kaldt");
-        return $http.get('http://dawa.aws.dk/adresser?q='+val, {   
- 
+        return $http.get('http://localhost:8080/ConvoyServer/webresources/convoy/get_dawa/'+val, {   
         }).then(function(response){
             $scope.adresser = response.data;
-        console.log($scope.adresser[0]);    
-        return $scope.adresser;
-           
+            return $scope.adresser;
         });
     };
     
     
     $scope.setkoord = function(adresse){
-            $scope.spot.longitude = adresse.adgangsadresse.adgangspunkt.koordinater[0];
-            console.log($scope.longitude);
-            $scope.spot.latitude = adresse.adgangsadresse.adgangspunkt.koordinater[1];
-            console.log($scope.latitude);
-    }
+        $scope.spot.longitude = adresse.adgangsadresse.adgangspunkt.koordinater[0];
+        console.log($scope.longitude);
+        $scope.spot.latitude = adresse.adgangsadresse.adgangspunkt.koordinater[1];
+        console.log($scope.latitude);
+    };
  
     //Efter submit overskrives form med tomme værdier 
     var tomForm = {
-        adresse: "",
+        adresser: "",
         stednavn: "",
         toilet : false,
         bad : false,
@@ -106,7 +109,7 @@ app.controller('spotController', function($scope, $http){
    
     $scope.submitData = function() {
         var jsonSpot = JSON.stringify($scope.spot);
-        console.log(jsonSpot);
+        
         console.log($scope.spot);
      
         
@@ -114,18 +117,27 @@ app.controller('spotController', function($scope, $http){
             method  : 'POST',
             url     : 'http://localhost:8080/ConvoyServer/webresources/convoy/create/spot',
             data    :  jsonSpot
-        })
-                .success(function(data) {
-                    window.alert("Spot er oprettet!");
+        }).success(function(data) {
+            window.alert("Spot er oprettet!");
+            console.log("Spot er oprettet!: "+ jsonSpot);
+     
             console.log($scope.test.spotForm.$pristine);
-            $scope.test.spotForm.$setPristine();    
+            $scope.test.spotForm.$setPristine();   
+            $scope.test.spotForm.adresser = "";
+                    
+                
+            
             $scope.spot = angular.copy(tomForm);
+            
+            
+             
+            
             console.log($scope.test.spotForm.$pristine); 
             
           
                     
             if (!data.success) {
-                console.log("Fejl ved gemning af data");
+                //console.log("Fejl ved gemning af data");
             }
         });
     };
