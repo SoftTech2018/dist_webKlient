11868594f1ca20fa2b1258570fc57d040c3a126d
camillaolsen <camilla.braae.olsen1@gmail.com>
2016-05-03 11:03:07 +0200

    Kodeoprydning samt klar indikation af felter der skal udfyldes i opretSpot

--- web/VisSpotController.js
+++ web/VisSpotController.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 
 var app = angular.module('jsonapp', ['ngRoute', 'booleanFilter','ngAnimate','ui.bootstrap']);
 
@@ -22,21 +15,12 @@ app.config(function ($routeProvider) {
 });
 
 
-
-
-
-
 app.controller('jsonController', function($http,$scope){
     $http({
         method: 'GET',
         url: 'http://localhost:8080/ConvoyServer/webresources/convoy/get_all'
     }).success(function(data){
         $scope.data = data;    
-        console.log($scope.data);
-        
-       
-        console.log(typeof($scope.id));
-        console.log($scope.id);
     });
 });
 
@@ -56,15 +40,12 @@ angular.module('booleanFilter', []).filter('checkmark', function() {
 });
 
 
-
-
 app.controller('spotController', function($scope, $http){
    
     $scope.test = {};
     $scope.spot = {};
     
     //Default værdier til objekt. Overskrives når form submittes. 
-   
     $scope.spot.wc = false;
     $scope.spot.bath = false;
     $scope.spot.food = false;
@@ -77,7 +58,6 @@ app.controller('spotController', function($scope, $http){
     
     
     $scope.getAdress = function(val) {
-        console.log("getADr er kaldt");
         return $http.get('http://localhost:8080/ConvoyServer/webresources/convoy/get_dawa/'+val, {   
         }).then(function(response){
             $scope.adresser = response.data;
@@ -85,12 +65,9 @@ app.controller('spotController', function($scope, $http){
         });
     };
     
-    
     $scope.setkoord = function(adresse){
         $scope.spot.longitude = adresse.adgangsadresse.adgangspunkt.koordinater[0];
-        console.log($scope.longitude);
         $scope.spot.latitude = adresse.adgangsadresse.adgangspunkt.koordinater[1];
-        console.log($scope.latitude);
     };
  
     //Efter submit overskrives form med tomme værdier 
@@ -110,38 +87,21 @@ app.controller('spotController', function($scope, $http){
     $scope.submitData = function() {
         var jsonSpot = JSON.stringify($scope.spot);
         
-        console.log($scope.spot);
-     
-        
         $http({
             method  : 'POST',
             url     : 'http://localhost:8080/ConvoyServer/webresources/convoy/create/spot',
             data    :  jsonSpot
-        }).success(function(data) {
+        }).then(function successCallback(data){
             window.alert("Spot er oprettet!");
             console.log("Spot er oprettet!: "+ jsonSpot);
-     
-            console.log($scope.test.spotForm.$pristine);
+
             $scope.test.spotForm.$setPristine();   
-            $scope.test.spotForm.adresser = "";
-                    
-                
-            
+            $scope.test.spotForm.adresser = "";         
             $scope.spot = angular.copy(tomForm);
-            
-            
-             
-            
-            console.log($scope.test.spotForm.$pristine); 
-            
-          
-                    
-            if (!data.success) {
-                //console.log("Fejl ved gemning af data");
-            }
+        }, function errorCallback(data) {
+            window.alert("Fejl i oprettelse af spot. Prøv igen!");
+        
         });
     };
 
 }); 
-
-
