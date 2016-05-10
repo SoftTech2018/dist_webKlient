100c133aa380291b61d088970744bd39e7787aed
camillaolsen <camilla.braae.olsen1@gmail.com>
2016-05-10 14:36:10 +0200

    CORS burde virke.. måske.

--- web/VisSpotController.js
+++ web/VisSpotController.js
@@ -1,7 +1,12 @@
 
 var app = angular.module('jsonapp', ['ngRoute', 'booleanFilter','ngAnimate','ui.bootstrap']);
 
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $httpProvider) {
+    
+    $httpProvider.defaults.useXDomain = true;
+  
+   
+    
     $routeProvider
             .when('/', {
                 templateUrl: 'sider/VisSpots.html',
@@ -15,82 +20,110 @@ app.config(function ($routeProvider) {
 });
 
 
+
+
 app.controller('jsonController', function($http,$scope){
     $http({
         method: 'GET',
-        url: 'http://localhost:8080/ConvoyServer/webresources/convoy/get_all'
-    }).success(function(data){
-        $scope.data = data;    
+        url: 'http://default-environment.xyik5z2rn3.eu-central-1.elasticbeanstalk.com/webresources/convoy/get_all',
+        
+    }).success(function(data1){
+        
+        
+        $scope.all = data1; 
+        console.log(data1); 
+        $scope.data = [];
+        angular.forEach($scope.all, function(item){
+            if(item.deleted === false){
+                $scope.data.push(item);
+                return $scope.data;
+            }
+        });
     });
+       
 });
 
 
 
+
+
+
 angular.module('booleanFilter', []).filter('checkmark', function() {
-    return function(input) {
-        var output;
+return function(input) {
+    var output;
  
-        if(input === true || input === false){
-            output = input ? '\u2713' : '\u2718';
-            return output;
-        }
-        else { return input;
-        }
-    };
+    if(input === true || input === false){
+        output = input ? '\u2713' : '\u2718';
+        return output;
+    }
+    else { return input;
+    }
+};
 });
 
 
 app.controller('spotController', function($scope, $http){
    
-    $scope.test = {};
-    $scope.spot = {};
+$scope.test = {};
+$scope.spot = {};
     
-    //Default værdier til objekt. Overskrives når form submittes. 
-    $scope.spot.wc = false;
-    $scope.spot.bath = false;
-    $scope.spot.food = false;
-    $scope.spot.bed = false;
-    $scope.spot.fuel = false;
-    $scope.spot.addBlue = false;
-    $scope.spot.roadtrain = false; 
-    $scope.spot.longitude = "";
-    $scope.spot.latitude = "";
+//Default værdier til objekt. Overskrives når form submittes. 
+$scope.spot.wc = false;
+$scope.spot.bath = false;
+$scope.spot.food = false;
+$scope.spot.bed = false;
+$scope.spot.fuel = false;
+$scope.spot.addBlue = false;
+$scope.spot.roadtrain = false; 
+$scope.spot.longitude = -1;
+$scope.spot.latitude = -1;
     
     
-    $scope.getAdress = function(val) {
-        return $http.get('http://localhost:8080/ConvoyServer/webresources/convoy/get_dawa/'+val, {   
-        }).then(function(response){
-            $scope.adresser = response.data;
-            return $scope.adresser;
-        });
-    };
+$scope.getAdress = function(val) {
+    return $http.get('http://default-environment.m2ypbqk78s.us-west-2.elasticbeanstalk.com/webresources/convoy/get_dawa/'+val, {   
+    }).then(function(response){
+        $scope.adresser = response.data;
+        return $scope.adresser;
+    });
+};
     
-    $scope.setkoord = function(adresse){
-        $scope.spot.longitude = adresse.adgangsadresse.adgangspunkt.koordinater[0];
-        $scope.spot.latitude = adresse.adgangsadresse.adgangspunkt.koordinater[1];
-    };
+    
+    
+$scope.setkoord = function(adresse){
+    $scope.spot.longitude = adresse.adgangsadresse.adgangspunkt.koordinater[0];
+    $scope.spot.latitude = adresse.adgangsadresse.adgangspunkt.koordinater[1];
+       
+};
  
-    //Efter submit overskrives form med tomme værdier 
-    var tomForm = {
-        adresser: "",
-        stednavn: "",
-        toilet : false,
-        bad : false,
-        mad : false,
-        seng : false,
-        tank : false,
-        ab : false,
-        vogntog : false
-    };
+//Efter submit overskrives form med tomme værdier 
+var tomForm = {
+    adresser: "",
+    stednavn: "",
+    toilet : false,
+    bad : false,
+    mad : false,
+    seng : false,
+    tank : false,
+    ab : false,
+    vogntog : false
+};
     
    
-    $scope.submitData = function() {
-        var jsonSpot = JSON.stringify($scope.spot);
-        
+$scope.submitData = function() {
+     var jsonSpot = JSON.stringify($scope.spot);
+   
+    
+//    var jsonSpot = JSON.stringify($scope.spot);
+//        
+    if($scope.spot.longitude === -1 || $scope.spot.latitude === -1){
+        window.alert("Fejl i adresse - vælg fra dropdown");
+    }
+    else{
         $http({
             method  : 'POST',
-            url     : 'http://localhost:8080/ConvoyServer/webresources/convoy/create/spot',
-            data    :  jsonSpot
+            url     : 'http://default-environment.xyik5z2rn3.eu-central-1.elasticbeanstalk.com/webresources/convoy/create/spot',
+            data    :  jsonSpot,
+            crossDomain : true
         }).then(function successCallback(data){
             window.alert("Spot er oprettet!");
             console.log("Spot er oprettet!: "+ jsonSpot);
@@ -102,6 +135,8 @@ app.controller('spotController', function($scope, $http){
             window.alert("Fejl i oprettelse af spot. Prøv igen!");
         
         });
-    };
+    }
+};
 
+    
 }); 
